8e817ca74a9a09643162208290fdeb1473c938a8
Liam Warbox <liam@warboxcreative.co.uk>
2023-08-15 11:30:49 +0100

    Use correct redirectUri params for Auth Provider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -17,7 +17,9 @@ export const wrapRootElement = ({ element }) => {
 		<Auth0Provider
 			domain={process.env.GATSBY_AUTH0_DOMAIN}
 			clientId={process.env.GATSBY_AUTH0_CLIENT_ID}
-			redirectUri={window.location.origin}
+			authorizationParams={{
+				redirect_uri: window.location.origin,
+			}}
 			onRedirectCallback={onRedirectCallback}
 		>
 			<UserProvider
