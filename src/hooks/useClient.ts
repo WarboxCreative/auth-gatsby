fed488534125311f8ec523c4d0c5e26a7b9b4c6e
Liam Warbox <liam@warboxcreative.co.uk>
2023-08-15 11:41:45 +0100

    Specify sanity API version and CDN useage

--- src/hooks/useClient.ts
+++ src/hooks/useClient.ts
@@ -5,5 +5,7 @@ export const useClient = () => {
 		projectId: process.env.GATSBY_SANITY_PROJECT_ID,
 		dataset: process.env.GATSBY_SANITY_PROJECT_DATASET,
 		token: process.env.GATSBY_SANITY_READ_TOKEN,
+		apiVersion: '2021-10-21',
+		useCdn: true,
 	})
 }
